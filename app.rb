7992d37bb3a4994688f9a034ed455174ad57a1ea
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-17 17:43:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! 
-    "
+    "Welcome to your app!!!! I BUILT THIS!"
   end
-
-
 
 end
 
